a973fa6d04cd77f392e7ce46c04319997a305dfc
Max Stevenson <maxstevenson@msn.com>
2018-11-13 16:45:05 +0000

    - Developed first page using TDD. - Players can now enter good their names in a form which is then posted to another page. - Rspec test working with capybara developed to test functionality.

--- spec/features/testing_infrastructure_spec.rb
+++ spec/features/testing_infrastructure_spec.rb
@@ -1,6 +1,11 @@
-feature 'Testing infrastructure' do
-  scenario 'Can run app and check page content' do
-    visit('/')
-    expect(page).to have_content 'Testing infrastructure working!'
+feature 'entering player names' do
+  scenario 'Users can enter their names' do
+  	player_1 = "Max"
+  	player_2 = "Ajay"
+  	visit('/')
+  	fill_in('player_1_name', with: "Max")
+  	fill_in('player_2_name', with: "Ajay")
+  	click_on('submit')
+  	expect(page).to have_content("Max vs. Ajay")
   end
 end
