e75c56c4abc0da65cb6fbe9428f2f08b82b019b1
Max Stevenson <maxstevenson@msn.com>
2018-11-14 12:51:32 +0000

    - Developed new test so that player 2 hitpoints are displayed in line with user story. - Added code to play.erb that displays hitpoints. - Refactor hitpoints as variable rather than magic number?

--- spec/features/testing_infrastructure_spec.rb
+++ spec/features/testing_infrastructure_spec.rb
@@ -9,3 +9,15 @@ feature 'entering player names' do
   	expect(page).to have_content("Max vs. Ajay")
   end
 end
+
+feature 'player hitpoints' do
+	scenario 'player 1 can view player 2 hitpoints' do
+		player_1 = "Max"
+  	player_2 = "Ajay"
+  	visit('/')
+  	fill_in('player_1_name', with: "Max")
+  	fill_in('player_2_name', with: "Ajay")
+  	click_on('submit')
+  	expect(page).to have_content("Ajay: 100HP")
+	end
+end
