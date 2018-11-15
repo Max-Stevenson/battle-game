2c1e9e5ff566122d8c26bde7896da2640d218d35
Max Stevenson <maxstevenson@msn.com>
2018-11-15 15:26:48 +0000

    - Refactored controller to utilise the newly created Game class.

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,9 +1,22 @@
 require 'game'
 
   describe Game do
-    player_2 = Player.new('Ajay')
+  	subject(:game) {described_class.new(player_1, player_2)}
+  	let(:player_1) {double :player}
+  	let(:player_2) {double :player}
+
+
     it "should be able to attack a player" do
       expect(player_2).to receive(:receive_damage)
-      Game.new.attack(player_2)
+      game.attack(player_2)
+    end
+
+    it "should retrieve the first player" do
+    	expect(game.player_1).to eq(player_1)
+    end
+
+    it "should retrieve the last player" do
+    	expect(game.player_2).to eq(player_2)
     end
   end
+
